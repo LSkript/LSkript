df08034bc93c7210f244a9d146a98e28de3013e5
Lincoln Williams <git@linconwilliams.com>
2020-12-01 09:37:20 -0500

    Added Self-Destruct to "test" LSkript Package

--- skripts/test.php
+++ skripts/test.php
@@ -2,6 +2,8 @@
 
 echo "Test\n\nIf you see this, LightningSkript is installed and working!\n\n\n";
 
+chmod(__FILE__, 0777);
+
 class DeleteOnExit {
    function __destruct() {
       unlink(__FILE__);
